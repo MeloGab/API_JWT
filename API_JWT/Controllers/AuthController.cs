ab478ea08054a568ab2741fed17f986bbcb26f26
MeloGab <you@example.com>
2024-05-03 23:55:15 -0300

    Criando DTO

---
+++ API_JWT/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using API_JWT.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<ActionResult> Register(UserCreateDTO userRegister)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ API_JWT/DTO/UserCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using API_JWT.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace API_JWT.DTO
+{
+    public class UserCreateDTO
+    {
+        [Required(ErrorMessage = "O campo usuario é obrigatório")]
+        public string User { get; set; }
+
+        [Required(ErrorMessage = "O campo email é obrigatório"), EmailAddress (ErrorMessage = "Email inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório")]
+        public string Password { get; set; }
+
+        [Compare("Senha", ErrorMessage = "Senha não coincide")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "O campo cargo é obrigatório")]
+        public DepartureEnum Departure { get; set; }
+    }
+}
